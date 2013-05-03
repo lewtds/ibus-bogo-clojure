4c56104376eb8683954c9d674cdd30a6a75ba4a8
Trung Ngo <ndtrung4419@gmail.com>
2013-05-04 00:02:05 +0700

    Rename: interpret-viqr -> parse-viqr

--- bogo/src/bogo/core.clj
+++ bogo/src/bogo/core.clj
@@ -1,5 +1,5 @@
 (ns bogo.core
-  (:require [bogo.accent :refer :all]
+  (:require [bogo.tone :refer :all]
             [bogo.mark :refer :all]
             [bogo.util :refer :all]))
 
@@ -16,13 +16,13 @@
             "j" ["."]
         })
 
-(defn interpret-viqr
+(defn parse-viqr
   "Parses an atomic VIQR-like string and returns a map indicating the necessary
   operation and arguments going with it."
   [string]
   (case (count string)
-    1 {:action :add-accent
-       :accent (case string
+    1 {:action :add-tone
+       :tone (case string
                   "`" :grave
                   "'" :acute
                   "?" :hook
@@ -41,20 +41,20 @@
         {:action nil}))))
 
 (defn execute-operation
-  "Executes the operation parsed by `interpret-viqr` on the given string."
+  "Executes the operation parsed by `parse-viqr` on the given string."
   [string operation]
   (case (operation :action)
-    :add-accent (add-accent-string string (operation :accent))
-    ; Backup and reapply the accent in these two cases since it's likely to be
+    :add-tone (add-tone-string string (operation :tone))
+    ; Backup and reapply the tone in these two cases since it's likely to be
     ; misplaced in previous operations.
-    (:add-mark :append-char) (add-accent-string
+    (:add-mark :append-char) (add-tone-string
       (case (operation :action)
         :add-mark (add-mark-string
-          (add-accent-string string :none)
+          (add-tone-string string :none)
           (operation :mark)
           (operation :target))
-        :append-char (str (add-accent-string string :none) (operation :char)))
-      (get-last-accent-string string))))
+        :append-char (str (add-tone-string string :none) (operation :char)))
+      (get-last-tone-string string))))
 
 (defn get-transformation-list
   "Finds the list of possible transformations written in a VIQR-like syntax that
@@ -70,10 +70,10 @@
       execute-operation
       string
       (map
-        interpret-viqr
+        parse-viqr
         (get-transformation-list (str chr) simple-telex)))]
     (if (= result string)
-      (execute-operation string (interpret-viqr (str "+" chr)))
+      (execute-operation string (parse-viqr (str "+" chr)))
       result)))
 
 (defn process-seq
