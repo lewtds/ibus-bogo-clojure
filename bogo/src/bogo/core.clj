722c81df221810a0e5c2579ed4664ea44bae36a3
Trung Ngo <ndtrung4419@gmail.com>
2013-05-05 01:22:25 +0700

    Don't reapply tone if the appended char is a word-boundary.

--- bogo/src/bogo/core.clj
+++ bogo/src/bogo/core.clj
@@ -40,6 +40,13 @@
         \+ {:action :append-char :char (last string)}
         {:action nil}))))
 
+(defmacro apply-with-tone
+  "doc-string"
+  [string function]
+  `(-> (add-tone-string ~string :none)
+      ~function
+      (add-tone-string (get-last-tone-string ~string))))
+
 (defn execute-operation
   "Executes the operation parsed by `parse-viqr` on the given string."
   [string operation]
@@ -47,14 +54,16 @@
     :add-tone (add-tone-string string (operation :tone))
     ; Backup and reapply the tone in these two cases since it's likely to be
     ; misplaced in previous operations.
-    (:add-mark :append-char) (add-tone-string
-      (case (operation :action)
-        :add-mark (add-mark-string
-          (add-tone-string string :none)
-          (operation :mark)
-          (operation :target))
-        :append-char (str (add-tone-string string :none) (operation :char)))
-      (get-last-tone-string string))))
+    :add-mark (apply-with-tone
+      (add-tone-string string :none)
+      (add-mark-string (operation :mark)
+                       (operation :target)))
+    :append-char (if (word-boundary? (operation :char))
+      ; Don't reapply tone if the appended char is a word-boundary.
+      (str string (operation :char))
+      (apply-with-tone
+        (add-tone-string string :none)
+        (str (operation :char))))))
 
 (defn get-transformation-list
   "Finds the list of possible transformations written in a VIQR-like syntax that
